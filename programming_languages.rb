b0dbcc375a066225166fc068cc3179010624e3b1
github username <github email address>
2017-08-03 15:46:19 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language_name|
+    language_name.each do |language_name, type|
+      type.each do |type, value|
+        new_hash[language_name] = {type: value, style: [style]}
+        if language_name == :javascript
+          new_hash[language_name] = {type: value, style: [:oo, :functional]}
+        end
+      end
+    end
+  end
+  new_hash
 end
